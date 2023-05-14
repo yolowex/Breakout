60c22361864df2b1725b11bb5d9fd7339c1e7ad6
Moein <pistolparody@gmail.com>
2023-05-14 21:13:56 +0330

    Updated project

--- Assets.py
+++ Assets.py
@@ -4,7 +4,8 @@ from pygame.math import Vector2 as Pos
 from pygame.rect import Rect
 from pygame.color import Color
 import random as r
-
+import os
+PATH = os.path.abspath('.')+'/'
 
 class Assets:
     def __init__(self):
@@ -14,18 +15,18 @@ class Assets:
         self.persian_font_path = "./fonts/farsi/farsi 3.ttf"
         self.english_font_path = "./fonts/english/FreeMonoBold.ttf"
 
-        self.bad_sound = pg.mixer.Sound("./sounds/bad_bonus.wav")
-        self.good_sound = pg.mixer.Sound("./sounds/Fruit collect 1.wav")
-        self.best_sound = pg.mixer.Sound("./sounds/Big Egg collect 1.wav")
+        self.bad_sound = pg.mixer.Sound(PATH+"sounds/bad_bonus.wav")
+        self.good_sound = pg.mixer.Sound(PATH+"sounds/Fruit collect 1.wav")
+        self.best_sound = pg.mixer.Sound(PATH+"sounds/Big Egg collect 1.wav")
 
-        self.hit_sounds = [pg.mixer.Sound("./sounds/" + i + ".wav") for i in
+        self.hit_sounds = [pg.mixer.Sound(PATH+"sounds/" + i + ".wav") for i in
             ["hit1", "hit2", "hit3", "hit4", "hit5"]]
 
-        self.gun_sounds = [pg.mixer.Sound("./sounds/" + i + ".wav") for i in
+        self.gun_sounds = [pg.mixer.Sound(PATH+"sounds/" + i + ".wav") for i in
             [f"laserShoot{i}" for i in range(1,4)]]
 
-        self.shoot_hit_sounds = [pg.mixer.Sound("./sounds/" + i + ".wav") for i in
+        self.shoot_hit_sounds = [pg.mixer.Sound(PATH+"sounds/" + i + ".wav") for i in
             [f"shoot_hit{i}" for i in range(1, 6)]]
 
-        self.break_sounds = [pg.mixer.Sound("./sounds/" + i + ".wav") for i in
+        self.break_sounds = [pg.mixer.Sound(PATH+"sounds/" + i + ".wav") for i in
             [f"hitHurt{i}" for i in range(1, 5)]]
